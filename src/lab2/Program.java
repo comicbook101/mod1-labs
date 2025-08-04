93f1dda34701d713c955ac0dfe4f9bdaa8774b55
comicbook101 <gpsage2010@gmail.com>
2025-08-04 14:10:40 +0100

    lab EG 01b completed

--- src/lab2/Program.java
+++ src/lab2/Program.java
@@ -23,32 +23,9 @@ public class Program {
 			System.out.println("Distinction");
 		}
 		else {
-			System.out.println("Please enter a number between 1 - 100");		}
-		txt.close();
-	}
-	
-	public static void ifStatement(String[] args) {
-		
-		Scanner txt = new Scanner(System.in);
-		System.out.println("What did you score on the test? ");
-		
-		int userResult = txt.nextInt();
-		
-		if (userResult <= 50) {
-			System.out.println("Failed");
-		} 
-		else if (userResult > 50 && userResult <= 60) {
-			System.out.println("Passed");
-		}
-		else if (userResult > 60 && userResult <= 70) {
-			System.out.println("Merit");
-		}
-		else if (userResult > 70 && userResult <= 100){
-			System.out.println("Distinction");
-		}
-		else {
 			System.out.println("Please enter a number between 1 - 100");
 		}
 		txt.close();
 	}
+	
 }
